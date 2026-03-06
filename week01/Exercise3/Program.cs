4dd0136313c6b8e3df4af6dea4caf9cb1c13206c
Paradis2019 <popol.elonda@byupw.edu>
2026-03-05 19:00:44 -0700

    upda

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // magic number
+        Console.Write("What is the magic number? ");
+        int magicNumber = int.Parse(Console.ReadLine());
+
+        // first guess
+        Console.Write("What is your guess? ");
+        int guess = int.Parse(Console.ReadLine());
+
+        // Loop till the guess is correct
+        while (guess != magicNumber)
+        {
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("You guessed it!");
     }
 }
