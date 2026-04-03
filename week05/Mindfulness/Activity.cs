526717b24efa3e6e5558cff6f80aa27d0e259d3d
Paradis2019 <popol.elonda@byupw.edu>
2026-04-03 17:52:55 -0600

    Update

--- week05/Mindfulness/Activity.cs
+++ week05/Mindfulness/Activity.cs
@@ -3,32 +3,57 @@ using System.Threading;
 
 public class Activity
 {
-    protected string _name;
-    protected string _description;
+    private string _name;
+    private string _description;
     protected int _duration;
+
+    public Activity(string name, string description)
+    {
+        _name = name;
+        _description = description;
+        _duration = 0;
+    }
+
+    public int GetDuration()
+    {
+        return _duration;
+    }
 
     public void DisplayStartingMessage()
     {
+        Console.Clear();
         Console.WriteLine($"Welcome to the {_name} Activity.");
+        Console.WriteLine();
         Console.WriteLine(_description);
-        Console.Write("Enter duration in seconds: ");
-        _duration = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+        Console.Write("How long, in seconds, would you like your session to be? ");
 
+        while (!int.TryParse(Console.ReadLine(), out _duration) || _duration <= 0)
+        {
+            Console.Write("Please enter a valid number greater than zero: ");
+        }
+
+        Console.WriteLine();
         Console.WriteLine("Get ready...");
         ShowSpinner(3);
+        Console.WriteLine();
     }
 
     public void DisplayEndingMessage()
     {
+        Console.WriteLine();
         Console.WriteLine("Well done!");
         ShowSpinner(3);
-        Console.WriteLine($"You completed {_duration} seconds of the {_name} Activity.");
+        Console.WriteLine();
+        Console.WriteLine($"You have completed {_duration} second(s) of the {_name} Activity.");
         ShowSpinner(3);
+        Console.WriteLine();
     }
 
     public void ShowSpinner(int seconds)
     {
         string[] spinner = { "|", "/", "-", "\\" };
+
         for (int i = 0; i < seconds * 4; i++)
         {
             Console.Write(spinner[i % 4]);
--- week05/Mindfulness/Reflectingactivity.cs
+++ week05/Mindfulness/Reflectingactivity.cs
@@ -5,43 +5,71 @@ public class ReflectingActivity : Activity
 {
     private List<string> _prompts = new List<string>()
     {
-        "Think of a time you overcame a challenge.",
-        "Think of a time you helped someone.",
-        "Think of a time you did something difficult."
+        "Think of a time when you stood up for someone else.",
+        "Think of a time when you did something really difficult.",
+        "Think of a time when you helped someone in need.",
+        "Think of a time when you did something truly selfless.",
+        "Think of a time when you overcame a fear."
     };
 
     private List<string> _questions = new List<string>()
     {
-        "Why was this experience meaningful?",
-        "What did you learn from it?",
-        "How did you feel afterward?"
+        "Why was this experience meaningful to you?",
+        "Have you ever done anything like this before?",
+        "How did you get started?",
+        "How did you feel when it was complete?",
+        "What made this time different than other times when you were not as successful?",
+        "What is your favorite thing about this experience?",
+        "What could you learn from this experience that applies to other situations?",
+        "What did you learn about yourself through this experience?",
+        "How can you keep this experience in mind in the future?"
     };
 
     private Random _random = new Random();
 
     public ReflectingActivity()
+        : base(
+            "Reflecting",
+            "This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.")
     {
-        _name = "Reflecting";
-        _description = "This activity helps you reflect on your strengths.";
     }
 
     public void Run()
     {
         DisplayStartingMessage();
 
-        string prompt = _prompts[_random.Next(_prompts.Count)];
-        Console.WriteLine($"\n{prompt}");
-        ShowSpinner(3);
+        Console.WriteLine("Consider the following prompt:");
+        Console.WriteLine();
+        Console.WriteLine($"--- {GetRandomPrompt()} ---");
+        Console.WriteLine();
+        Console.WriteLine("When you have something in mind, press Enter to continue.");
+        Console.ReadLine();
 
-        int time = 0;
-        while (time < _duration)
+        Console.WriteLine();
+        Console.WriteLine("Now ponder on each of the following questions as they relate to this experience.");
+        Console.Write("You may begin in: ");
+        ShowCountDown(5);
+        Console.WriteLine();
+
+        DateTime endTime = DateTime.Now.AddSeconds(GetDuration());
+
+        while (DateTime.Now < endTime)
         {
-            string question = _questions[_random.Next(_questions.Count)];
-            Console.WriteLine($"\n{question}");
-            ShowSpinner(4);
-            time += 4;
+            Console.Write($"{GetRandomQuestion()} ");
+            ShowSpinner(5);
+            Console.WriteLine();
         }
 
         DisplayEndingMessage();
+    }
+
+    public string GetRandomPrompt()
+    {
+        return _prompts[_random.Next(_prompts.Count)];
+    }
+
+    public string GetRandomQuestion()
+    {
+        return _questions[_random.Next(_questions.Count)];
     }
 }
